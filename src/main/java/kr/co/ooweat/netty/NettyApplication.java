cb0d17e4444826f61cec4f641bc054d2d574abea
ooweat <twkim326@gmail.com>
2022-09-13 14:27:16 +0900

    [ADD] Spring Boot 2.5.5 최초 생성

---
+++ src/main/java/kr/co/ooweat/netty/NettyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ooweat.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyApplication.class, args);
+	}
+
+}
